d33e3f5795d7f797448f958c3cf7b2f26ba4f356
BuiTuanVu <vubui2010@gmail.com>
2020-01-05 19:32:08 +0700

    update

--- readerui/src/Components/Detai/Detail.jsx
+++ readerui/src/Components/Detai/Detail.jsx
@@ -13,115 +13,92 @@ import { connect } from 'react-redux';
 const { Text } = Typography;
 
 class Detail extends Component {
-  state = { id: 0, recPost: {} };
-  componentWillMount() {
-    const values = queryString.parse(this.props.location.search);
-    console.log(values); // "top"
-    console.log(values.post); // "im"
-    const { postsData } = this.props;
-    console.log('date', postsData);
-    const id = values.post;
-    console.log(id);
-    const recPost = postsData.filter(item => item.id === Number(id))[0];
-    this.setState({ recPost: recPost });
-    console.log(recPost);
-  }
-  render() {
-    const { recPost } = this.state;
+	state = { id: 0, recPost: {} };
 
-    const dataTag = [
-      'Lý Hoàng Nam',
-      'SEA Games 30',
-      'HCV',
-      'Thể thao Việt Nam'
-    ];
-    return (
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <Breadcrumb style={{ margin: '20px 0px' }}>
-          <Breadcrumb.Item>
-            {' '}
-            <Link to="/home">
-              <Icon type="home"></Icon>&ensp;Trang chủ
-            </Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Link to="/bycate">Thể thao</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>{recPost.title}</Breadcrumb.Item>
-        </Breadcrumb>
+	render() {
+		const values = queryString.parse(this.props.location.search);
+		const { postsData } = this.props;
+		console.log('date', postsData);
+		const id = values.post;
 
-        <div
-          style={{ width: '100%', display: 'flex', flexDirection: 'column' }}
-        >
-          <Text strong style={{ fontSize: 20, color: 'black' }}>
-            {recPost.title}
-          </Text>
+		const recPost = postsData.filter((item) => item.id === Number(id))[0];
 
-          <Text style={{ margin: '20px 0px' }}>
-            <span style={{ fontWeight: 500, color: 'black' }}>
-              {recPost.writer}
-            </span>
-            &ensp;đăng lúc{' '}
-            <span style={{ fontWeight: 500, color: 'black' }}>
-              {recPost.date}
-            </span>
-            &nbsp;-&nbsp;
-            <span style={{ fontWeight: 'bold', color: 'orange' }}>
-              {recPost.cate}
-            </span>
-          </Text>
-          <Text
-            style={{ fontSize: 16, color: 'black', marginBottom: 10 }}
-            strong
-          >
-            {recPost.des}
-          </Text>
-          <div style={{ width: '100%' }}>{recPost.content}</div>
+		const dataTag = [ 'Lý Hoàng Nam', 'SEA Games 30', 'HCV', 'Thể thao Việt Nam' ];
+		return (
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				<Breadcrumb style={{ margin: '20px 0px' }}>
+					<Breadcrumb.Item>
+						{' '}
+						<Link to="/home">
+							<Icon type="home" />&ensp;Trang chủ
+						</Link>
+					</Breadcrumb.Item>
+					<Breadcrumb.Item>
+						<Link to="/bycate">Thể thao</Link>
+					</Breadcrumb.Item>
+					<Breadcrumb.Item>{recPost.title}</Breadcrumb.Item>
+				</Breadcrumb>
 
-          <div
-            style={{
-              margin: '20px 0px',
-              display: 'flex',
-              flexDirection: 'row'
-            }}
-          >
-            <Button
-              type="default"
-              style={{
-                backgroundColor: 'black',
-                color: 'white',
-                border: 'none'
-              }}
-            >
-              <Icon type="tags"></Icon>&nbsp;Từ khóa:
-            </Button>
-            <div>
-              {dataTag.map(item => (
-                <Tag
-                  className="tag"
-                  style={{
-                    fontSize: 16,
-                    margin: 5,
-                    height: '25px',
-                    lineHeight: '25px'
-                  }}
-                >
-                  <Link to="#" className="tag">
-                    {item}
-                  </Link>
-                </Tag>
-              ))}
-            </div>
-          </div>
-          <CommentForm />
-        </div>
-        <RelatedPostList />
-      </div>
-    );
-  }
+				<div style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
+					<Text strong style={{ fontSize: 20, color: 'black' }}>
+						{recPost.title}
+					</Text>
+
+					<Text style={{ margin: '20px 0px' }}>
+						<span style={{ fontWeight: 500, color: 'black' }}>{recPost.writer}</span>
+						&ensp;đăng lúc <span style={{ fontWeight: 500, color: 'black' }}>{recPost.date}</span>
+						&nbsp;-&nbsp;
+						<span style={{ fontWeight: 'bold', color: 'orange' }}>{recPost.cate}</span>
+					</Text>
+					<Text style={{ fontSize: 16, color: 'black', marginBottom: 10 }} strong>
+						{recPost.des}
+					</Text>
+					<div style={{ width: '100%' }}>{recPost.content}</div>
+
+					<div
+						style={{
+							margin: '20px 0px',
+							display: 'flex',
+							flexDirection: 'row'
+						}}
+					>
+						<Button
+							type="default"
+							style={{
+								backgroundColor: 'black',
+								color: 'white',
+								border: 'none'
+							}}
+						>
+							<Icon type="tags" />&nbsp;Từ khóa:
+						</Button>
+						<div>
+							{dataTag.map((item) => (
+								<Tag
+									className="tag"
+									style={{
+										fontSize: 16,
+										margin: 5,
+										height: '25px',
+										lineHeight: '25px'
+									}}
+								>
+									<Link to="#" className="tag">
+										{item}
+									</Link>
+								</Tag>
+							))}
+						</div>
+					</div>
+					<CommentForm />
+				</div>
+				<RelatedPostList />
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => ({
-  postsData: state.PostReducer.postsData
+const mapStateToProps = (state) => ({
+	postsData: state.PostReducer.postsData
 });
 export default connect(mapStateToProps, null)(withRouter(Detail));
--- readerui/src/Components/ListByCate/ListByCate.jsx
+++ readerui/src/Components/ListByCate/ListByCate.jsx
@@ -11,32 +11,32 @@ import RightContent from '../Commons/RightContent/RightContent';
 import Detail from '../Detai/Detail';
 
 class ListByCate extends Component {
-  render() {
-    return (
-      <div style={{ width: '100%' }}>
-        <Row gutter={30} style={{ marginTop: '-10px' }}>
-          <Col span={16}>
-            <Route exact path="/bycate/">
-              <Breadcrumb style={{ marginTop: 20 }}>
-                <Breadcrumb.Item>
-                  <Link to="/home">
-                    <Icon type="home"></Icon>&ensp;Trang chủ
-                  </Link>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>Thể thao</Breadcrumb.Item>
-              </Breadcrumb>
-              <OutstandingListByCate />
-            </Route>
-            <Route path="/bycate/detail">
-              <Detail />
-            </Route>
-          </Col>
-          <Col span={8} style={{ marginTop: '25px' }}>
-            <RightContent />
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={{ width: '100%' }}>
+				<Row gutter={30} style={{ marginTop: '-10px' }}>
+					<Col span={16}>
+						<Route exact path="/bycate/">
+							<Breadcrumb style={{ marginTop: 20 }}>
+								<Breadcrumb.Item>
+									<Link to="/home">
+										<Icon type="home" />&ensp;Trang chủ
+									</Link>
+								</Breadcrumb.Item>
+								<Breadcrumb.Item>Thể thao</Breadcrumb.Item>
+							</Breadcrumb>
+							<OutstandingListByCate />
+						</Route>
+						<Route path="/bycate/detail">
+							<Detail />
+						</Route>
+					</Col>
+					<Col span={8} style={{ marginTop: '25px' }}>
+						<RightContent />
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 export default ListByCate;
